3699fab644c34208addf5cafd89fc03d9b673576
puttajogeswar <jogeswar.530@gmail.com>
2024-10-25 12:03:08 +0530

    check given number is even or odd.c

---
+++ check given number is even or odd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter n value ");
+    scanf("%d",&n);
+    if(n%2==0){
+        printf("Given number is even");
+    }
+    else{
+        printf("Given number is odd");
+    }
+}
