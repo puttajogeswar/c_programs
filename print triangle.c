d77d2488ceccbb6e78b61fcf92d2b17257d2074e
puttajogeswar <jogeswar.530@gmail.com>
2025-01-03 11:54:47 +0530

    Create print triangle.c

---
+++ print triangle.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+   int i,j;
+   int n=1;
+   for(i=1;i<=5;i++){
+       for(j=1;j<=i;j++){
+           printf("* ");
+       }
+       printf("\n");
+   }
+   for(i=5;i>=1;i--){
+       for(j=1;j<=i;j++){
+           printf("* ");
+       }
+       printf("\n");
+   }
+return 0;
+}
