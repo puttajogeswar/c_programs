680a735831734f43c2c3d14718ea438c22a5f1c1
puttajogeswar <jogeswar.530@gmail.com>
2025-01-03 11:52:31 +0530

    Create print * in reverse.c

---
+++ print * in reverse.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+   int i,j;
+   int n=1;
+   for(i=5;i>=1;i--){
+       for(j=1;j<=i;j++){
+           printf("* ");
+       }
+       printf("\n");
+   }
+return 0;
+}
